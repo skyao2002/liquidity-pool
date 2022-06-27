62cab5e28fcd10e632dc8aa710d7a65e82ab1365
Steven Yao <syaointern@69g19xd.w2k.jumptrading.com>
2022-06-27 09:34:53 -0500

    bread init

--- src/BreadSwap.sol
+++ src/BreadSwap.sol
@@ -11,12 +11,12 @@ error WrongAssets();
 contract BreadSwap {
     BreadCoin public bread;
 
-    uint64 public totalShares;
+    uint public totalShares;
     address public tokenA;
     address public tokenB;
-    uint64 public totalTokenA;
-    uint64 public totalTokenB;
-    uint64 public K;
+    uint public totalTokenA;
+    uint public totalTokenB;
+    uint public K;
 
     constructor() {
         bread = new BreadCoin();
@@ -26,9 +26,13 @@ contract BreadSwap {
         return bread.balanceOf(msg.sender);
     }
 
+    function getTVL(address _token) public view returns (uint256) {
+        return IERC20(_token).balanceOf(address(this));
+    }
+
     // user must first approve the amount of each token to be transferred by this contract
-    function deposit(address _tokenA, address _tokenB, uint64 _amtTokenA, uint64 _amtTokenB) public {
-        uint64 userShares = 0;
+    function deposit(address _tokenA, address _tokenB, uint _amtTokenA, uint _amtTokenB) public {
+        uint userShares = 0;
         if (totalShares == 0) {
             // case when first deposit is triggered
             userShares = 100;
@@ -37,8 +41,8 @@ contract BreadSwap {
         } else if (tokenA != _tokenA || tokenB != _tokenB) {
             revert WrongAssets();
         } else {
-            uint64 shares_tokenA = (totalShares * _amtTokenA)/ totalTokenA;
-            uint64 shares_tokenB = (totalShares * _amtTokenB)/ totalTokenB;
+            uint shares_tokenA = (totalShares * _amtTokenA)/ totalTokenA;
+            uint shares_tokenB = (totalShares * _amtTokenB)/ totalTokenB;
             if (shares_tokenA != shares_tokenB) {
                 revert BadRatio();
             }
@@ -60,7 +64,70 @@ contract BreadSwap {
         bread.mint(msg.sender, uint(userShares));
     }
 
-    function helloworld() public pure returns (string memory) {
-        return "hello world";
+    function getEquivalentTokenEsitmate(address _token, uint _amt) public view returns (uint) {
+        if (_token != tokenA && _token != tokenB) {
+            revert WrongAssets();
+        } else if (_token == tokenA) {
+            return totalTokenB * _amt / totalTokenA;
+        } else {
+            return totalTokenA * _amt / totalTokenB;
+        }
+    }
+
+    function withdraw(uint _shares) public {
+        // check that the user has enough shares to withdraw
+        require(_shares > 0, "Must withdraw more than 0 shares.");
+        require(bread.balanceOf(msg.sender) >= _shares, "Do not own enough shares to withdraw!");
+
+        uint amtTokenA = _shares * totalTokenA / totalShares;
+	    uint amtTokenB = _shares * totalTokenB / totalShares;
+
+        bread.burn(msg.sender, _shares);
+        IERC20(tokenA).transfer(msg.sender, amtTokenA);
+        IERC20(tokenB).transfer(msg.sender, amtTokenB);
+
+        totalShares -= _shares;
+        totalTokenA -= amtTokenA;
+        totalTokenB -= amtTokenB;
+        K = totalTokenA*totalTokenB;
+    }
+
+    function swap(address _token, uint _amt) public {
+        if (_token != tokenA && _token != tokenB) {
+            revert WrongAssets();
+        } else if (_token == tokenA) {
+            uint tokenA_postSwap = totalTokenA + _amt;
+            uint totalB_postSwap = K/tokenA_postSwap;
+
+            uint amtTokenB = totalTokenB - totalB_postSwap;
+
+            if (amtTokenB == totalTokenB) {
+                amtTokenB -= 1;
+            }
+
+            IERC20(tokenA).transferFrom(msg.sender, address(this), _amt);
+            totalTokenA += _amt;
+
+            IERC20(tokenB).transfer(msg.sender, amtTokenB);
+            totalTokenB -= amtTokenB;
+
+        } else {
+            uint tokenB_postSwap = totalTokenB + _amt;
+            uint totalA_postSwap = K/tokenB_postSwap;
+
+            uint amtTokenA = totalTokenA - totalA_postSwap;
+
+            if (amtTokenA == totalTokenA) {
+                amtTokenA -= 1;
+            }
+
+            IERC20(tokenB).transferFrom(msg.sender, address(this), _amt);
+            totalTokenB += _amt;
+
+            IERC20(tokenA).transfer(msg.sender, amtTokenA);
+            totalTokenA -= amtTokenA;
+        }
+
+        
     }
 }
--- test/BreadSwap.t.sol
+++ test/BreadSwap.t.sol
@@ -10,44 +10,96 @@ contract AmmTest is Test {
     Sour public establishedSRB;
     Rye public establishedRIB;
 
+    address p1;
+    address p2;
+    address p3;
+
     function setUp() public {
         amm = new BreadSwap();
         establishedSRB = new Sour();
         establishedRIB = new Rye();
-        establishedSRB.mint(address(this), 10000);
-        establishedRIB.mint(address(this), 10000);
+        p1 = address(this);
+        p2 = address(0x102);
+        p3 = address(0x103);
+
+        establishedSRB.mint(p1, 10000);
+        establishedRIB.mint(p1, 10000);
+
+        establishedSRB.mint(p2, 10000);
+        establishedRIB.mint(p2, 10000);
+
+        establishedSRB.mint(p3, 10000);
+        establishedRIB.mint(p3, 10000);
     }
 
-    function testSanity() public {
-        string memory s = amm.helloworld();
-        assertEq(s, "hello world");
-    }
-
-    function testDeposit() public {
+    function testBread() public {
+        // starting the pool with 5:6 ratio
         establishedSRB.approve(address(amm), 50);
         establishedRIB.approve(address(amm), 60);
         amm.deposit(address(establishedSRB), address(establishedRIB), 50, 60);
         assertEq(amm.getMyBalance(), 100);
 
+        // increasing supply to 55:66
         establishedSRB.approve(address(amm), 5);
         establishedRIB.approve(address(amm), 6);
         amm.deposit(address(establishedSRB), address(establishedRIB),5,6);
         // deposit / totalA = 5 / 50 = 0.1 of the pool; thus 0.1*100 = 10
         assertEq(amm.getMyBalance(), 110);
         
+        // test that bad ration will fail
         establishedSRB.approve(address(amm), 10);
         establishedRIB.approve(address(amm), 20);
         vm.expectRevert(BadRatio.selector);
         amm.deposit(address(establishedSRB), address(establishedRIB),10,20);
 
+        // increase supply to 70:84 from different supplier p2
+        vm.prank(p2);
         establishedSRB.approve(address(amm), 15);
+        vm.prank(p2);
         establishedRIB.approve(address(amm), 18);
+        vm.prank(p2);
         amm.deposit(address(establishedSRB), address(establishedRIB),15,18);
-        assertEq(amm.getMyBalance(), 140);
+        vm.prank(p2);
+        assertEq(amm.getMyBalance(), 30);
 
+        // test using a token not in the pool will fail
         establishedSRB.approve(address(amm), 5);
         establishedRIB.approve(address(amm), 6);
         vm.expectRevert(WrongAssets.selector);
         amm.deposit(address(establishedSRB), address(100000),5,6);
+
+        // TVL locked by pool of tokenA is 70
+        assertEq(amm.getTVL(address(establishedSRB)), 70);
+
+        // to contribute 30 of tokenA, you must contribute 36 of tokenB
+        assertEq(amm.getEquivalentTokenEsitmate(address(establishedSRB), 30), 36);
+
+        // now lets say that tokenA starts to crash, people want to swap out tokenA for tokenB
+        vm.prank(p3);
+        establishedSRB.approve(address(amm), 35);
+        vm.prank(p3);
+        amm.swap(address(establishedSRB), 35);
+        // K = 70*84 = 5880 ; tokenA post swap = 105 ; token B postswap = 5880 / 105 = 56 ; token B recieved = 84-56 = 28
+        assert(establishedSRB.balanceOf(p3) == 9965 && establishedRIB.balanceOf(p3) == 10028);
+
+
+        // now tokenA spikes, people want to swap tokenB for tokenA
+        vm.prank(p3);
+        establishedRIB.approve(address(amm), 100);
+        vm.prank(p3);
+        amm.swap(address(establishedRIB), 100);
+        // K = 105*56 = 5880 ; tokenB post swap = 156 ; token A postswap = 5880 / 156 = 37 ; token A recieved = 105-37 = 68
+        assertEq(establishedSRB.balanceOf(p3), 10033);
+        assertEq(establishedRIB.balanceOf(p3), 9928);
+
+
+        // p1 wants to withdraw 80 shares
+        amm.withdraw(80);
+        // tokenA withdrawn = 80 * 37 / 140 = 21
+        // tokenB withdrawn = 89
+        // the supplier ended with more tokenB even though it is currently more favorable to own tokenA
+        assertEq(establishedSRB.balanceOf(p1), 10000-55+21);
+        assertEq(establishedRIB.balanceOf(p1), 10000-66+89);
+        assertEq(amm.getMyBalance(), 30);
     }
 }
